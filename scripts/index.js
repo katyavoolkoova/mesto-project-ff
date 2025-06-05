bde5e102b388f0370d95a8a46f7d76affbae9828
Ekaterina Zakharova <inst.katyavoolkoova@yandex.ru>
2025-06-05 13:40:48 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+function createCard(card, removeNodeCallback) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
+
+  buttonDelete.addEventListener("click", function () {
+    removeNodeCallback(cardElement);
+  });
+
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+
+  console.log(removeNodeCallback);
+  return cardElement;
+}
+
+initialCards.forEach(function (item) {
+  const cards = createCard(item, deleteCard);
+  placesList.append(cards);
+});
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
